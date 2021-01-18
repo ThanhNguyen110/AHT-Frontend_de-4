d1d9031f866f5916b1a514d037151092d09bddfc
ThanhNguyen110 <thanhnv.hn110@gmail.com>
2021-01-18 17:49:30 +0700

    commit

---
+++ js/owl.js
@@ -0,0 +1,17 @@
+
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  autoplay:false,
+  autoplayTimeout:3000,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 1
+    }
+  }
+});
